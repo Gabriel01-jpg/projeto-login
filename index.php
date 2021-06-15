12ceec40cfd783a90422094b15ba3386abf95377
Gabriel Lima <gabriellima170801@gmail.com>
2021-06-15 18:30:52 -0300

    Versão inicial

---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style-login.css">
+    <title>Login</title>
+</head>
+<body>
+    <div class="background-page">
+        <div class="box-login">
+            <form action="login.php" method="POST">
+                <div class="login-input">
+                    <input type="text" placeholder="Login:" name="login">
+                </div>
+                <div class="password-input">
+                    <input type="password" placeholder="Password:" name="password">
+                </div>
+                <div class="submit-login">
+                    <button value="1" type="submit" name="submit-php">Entrar</button>
+                </div>
+            </form>
+                
+            
+        </div>
+    </div>
+    
+</body>
+</html>
